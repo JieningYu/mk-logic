e2b136caf1f7c4377815217d73aa843332b28e28
JieningYu <jiening.yu@outlook.com>
2026-04-22 19:55:32 +0800

    excrete

--- MkLogic/Basic.lean
+++ MkLogic/Basic.lean
@@ -46,10 +46,10 @@ theorem Union.idem_eq : x ∪ x = x := Class.eq fun _ => Union.idem
 theorem Inter.idem : y ∈ x ∩ x ↔ y ∈ x := by simp
 theorem Inter.idem_eq : x ∩ x = x := Class.eq fun _ => Inter.idem
 
-@[symm] theorem Union.comm : z ∈ x ∪ y → z ∈ y ∪ x := fun h => Union.intro (Union.split h).symm
+@[symm, simp] theorem Union.comm : z ∈ x ∪ y → z ∈ y ∪ x := fun h => Union.intro (Union.split h).symm
 theorem Union.comm_eq : x ∪ y = y ∪ x := Class.eq fun _ => Iff.intro Union.comm Union.comm
 
-@[symm] theorem Inter.comm : z ∈ x ∩ y → z ∈ y ∩ x := fun h => Inter.intro (Inter.split h).symm
+@[symm, simp] theorem Inter.comm : z ∈ x ∩ y → z ∈ y ∩ x := fun h => Inter.intro (Inter.split h).symm
 theorem Inter.comm_eq : x ∩ y = y ∩ x := Class.eq fun _ => Iff.intro Inter.comm Inter.comm
 
 private theorem Union.assoc_oneshot : w ∈ (x ∪ y) ∪ z → w ∈ x ∪ (y ∪ z) := fun h => Or.elim (Union.split h)
@@ -164,7 +164,10 @@ theorem Sub.split : z ∈ x - y → z ∈ x ∧ z ∉ y := fun h =>
 @[simp,symm] theorem Sub.inter_assoc : w ∈ x ∩ (y - z) ↔ w ∈ (x ∩ y) - z := Inter.assoc.symm
 theorem Sub.inter_assoc_eq : x ∩ (y - z) = (x ∩ y) - z := Class.eq fun _ => Sub.inter_assoc
 
-noncomputable abbrev Φ := Classify fun x => x ≠ x
+noncomputable instance : EmptyCollection Class where
+  emptyCollection := Classify fun x => x ≠ x
+
+noncomputable abbrev Φ : Class := ∅
 
 theorem Class.not_in_empty : x ∉ Φ := Not.intro fun fake => (Class.classify.mp fake).right rfl
 
@@ -175,5 +178,88 @@ theorem Union.elim_empty_eq : Φ ∪ x = x := Class.eq fun _ => Union.elim_empty
 
 @[simp] theorem Inter.elim_empty : y ∈ Φ ∩ x ↔ y ∈ Φ := Iff.intro
   (fun h => And.left (Inter.split h))
-  (fun h => Classical.byContradiction fun _ => Class.not_in_empty h)
+  (fun h => (Class.not_in_empty h).elim)
 theorem Inter.elim_empty_eq : Φ ∩ x = Φ := Class.eq fun _ => Inter.elim_empty
+
+noncomputable def μ := Classify fun x => x = x
+
+theorem Class.in_complete : x ∈ μ ↔ Ensemble x := Iff.intro
+  (fun h => And.left (Class.classify.mp h))
+  (fun h => Class.classify.mpr ⟨h, rfl⟩)
+
+@[simp] theorem Union.elim_complete : y ∈ x ∪ μ ↔ y ∈ μ := Iff.intro
+  (fun h => Class.in_complete.mpr ⟨x ∪ μ, h⟩)
+  (fun h => Union.intro (Or.inr h))
+theorem Union.elim_complete_eq : x ∪ μ = μ := Class.eq fun _ => Union.elim_complete
+
+@[simp] theorem Inter.elim_complete : y ∈ x ∩ μ ↔ y ∈ x := Iff.intro
+  (fun h => And.left (Inter.split h))
+  (fun h => Inter.intro ⟨h, Class.in_complete.mpr ⟨x, h⟩⟩)
+theorem Inter.elim_complete_eq : x ∩ μ = x := Class.eq fun _ => Inter.elim_complete
+
+theorem complete_compl_empty : x ∈ μ ↔ x ∈ ~~~Φ := Iff.intro
+  (fun h => Class.classify.mpr ⟨⟨μ, h⟩, Class.not_in_empty⟩)
+  (fun h => Class.in_complete.mpr ⟨~~~Φ, h⟩)
+theorem complete_compl_empty_eq : μ = ~~~Φ := Class.eq fun _ => complete_compl_empty
+
+theorem empty_compl_complete : x ∈ Φ ↔ x ∈ ~~~μ := Iff.intro
+  (fun h => (Class.not_in_empty h).elim)
+  (fun h => have ⟨ensx, not⟩ := Class.classify.mp h; (not (Class.in_complete.mpr ensx)).elim)
+theorem empty_compl_complete_eq : Φ = ~~~μ := Class.eq fun _ => empty_compl_complete
+
+noncomputable def SInter (x : Class) := Classify fun z => ∀ y, y ∈ x → z ∈ y
+noncomputable def SUnion (x : Class) := Classify fun z => ∃ y, z ∈ y ∧ y ∈ x
+
+@[simp] theorem sinter_empty_is_complete : x ∈ SInter Φ ↔ x ∈ μ := Iff.intro
+  (fun h => Class.in_complete.mpr ⟨SInter Φ, h⟩)
+  (fun h => Class.classify.mpr ⟨⟨μ, h⟩, fun _ fake => (Class.not_in_empty fake).elim⟩)
+theorem sinter_empty_is_complete_eq : SInter Φ = μ := Class.eq fun _ => sinter_empty_is_complete
+
+@[simp] theorem sunion_empty_is_empty : x ∈ SUnion Φ ↔ x ∈ Φ := Iff.intro
+  (fun h => have ⟨_, ⟨_, h1⟩⟩ := Class.classify.mp h; (Class.not_in_empty h1.right).elim)
+  (fun h => (Class.not_in_empty h).elim)
+theorem sunion_empty_is_empty_eq : SUnion Φ = Φ := Class.eq fun _ => sunion_empty_is_empty
+
+instance : HasSubset Class where
+  Subset x y := ∀ z, z ∈ x → z ∈ y
+
+@[simp] theorem empty_is_subset : Φ ⊆ x := fun _ h => (Class.not_in_empty h).elim
+@[simp] theorem complete_is_superset : x ⊆ μ := fun _ h => Class.in_complete.mpr ⟨x, h⟩
+@[refl, simp] theorem subset_rfl : x ⊆ x := fun _ => id 
+
+@[simp, symm] theorem subset_antisymm : x ⊆ y ∧ y ⊆ x ↔ x = y := Iff.intro
+  (fun ⟨h1, h2⟩ => Class.eq fun z => Iff.intro (fun zsx => h1 z zsx) (fun zsy => h2 z zsy))
+  (fun h => by rw[h]; simp)
+
+@[simp] theorem subset_with_union : x ⊆ y → x ⊆ y ∪ z := fun h w wsx => Union.intro (Or.inl (h w wsx))
+@[simp] theorem inter_is_subset : x ∩ y ⊆ x := fun _ h => And.left (Inter.split h)
+
+@[simp] theorem subset_trans : x ⊆ y → y ⊆ z → x ⊆ z := fun h1 h2 w zsx => h2 w (h1 w zsx)
+
+@[simp] theorem union_absorb_subset : x ∪ y = y ↔ x ⊆ y := Iff.intro
+  (fun h => by rw[←h]; simp)
+  (fun h => Class.eq fun z => Iff.intro
+    (fun zsxy => (Union.split zsxy).elim (fun zsx => h z zsx) id)
+    (fun zsy => Union.intro (Or.inr zsy)))
+@[simp] theorem inter_absorb_subset : x ∩ y = x ↔ x ⊆ y := Iff.intro
+  (fun h => by rw[←h]; rw[Inter.comm_eq]; simp)
+  (fun h => Class.eq fun z => Iff.intro
+    (fun zsxy => And.left (Inter.split zsxy))
+    (fun zsx => Inter.intro ⟨zsx, h z zsx⟩))
+
+theorem union_monotone : x ⊆ y → SUnion x ⊆ SUnion y := fun h _ zsux =>
+  have ⟨ensz, ⟨w, ⟨zsw, wsx⟩⟩⟩ := Class.classify.mp zsux
+  Class.classify.mpr ⟨ensz, ⟨w, ⟨zsw, h w wsx⟩⟩⟩
+theorem inter_monotone : x ⊆ y → SInter y ⊆ SInter x := fun h _ zsiy =>
+  have ⟨ensz, h1⟩ := Class.classify.mp zsiy
+  Class.classify.mpr ⟨ensz, fun w wsx => have wsy := h w wsx; h1 w wsy⟩
+
+theorem union_monotone_mem : x ∈ y → x ⊆ SUnion y := fun xsy _ zsx  =>
+  Class.classify.mpr ⟨⟨x, zsx⟩, ⟨x, ⟨zsx, xsy⟩⟩⟩
+theorem inter_monotone_mem : x ∈ y → SInter y ⊆ x := fun xsy _ zsiy =>
+  And.right (Class.classify.mp zsiy) x xsy
+
+noncomputable def pow (x : Class) := Classify fun y => y ⊆ x
+
+-- axiom Class.subsets : ∀ x, Ensemble x → ∃ y, Ensemble y ∧ pow x ⊆ y
+axiom Class.subsets : ∀ x, Ensemble x → ∃ y, Ensemble y ∧ (∀ z, z ⊆ x -> z ∈ y)
